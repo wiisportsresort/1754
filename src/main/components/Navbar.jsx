dec9504d5e63dbef8fc8243ccde2195b5e3eb889
wiisportsresort <devdoge1@gmail.com>
2020-04-08 10:08:05 -0400

    hexupdate listener

--- src/main/components/Navbar.jsx
+++ src/main/components/Navbar.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-const capitalize = string => string[0].toUpperCase() + string.slice(1);
+import { EventPipe } from '../../common/event';
+import { colors } from '../../common/hexData';
 import { capitalize } from '../../common/util';
 
 export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { counts: {} };
+
+    for (let group of Object.keys(colors)) {
+      this.state.counts[group] = this.props.initialOwnedHexes[group].length;
+    }
+  }
+  componentDidMount() {
+    this.props.eventPipe.on('hexupdate', event => {
+      const { oldOwner, newOwner } = event.detail;
+      this.setState((state, _props) => {
+        state.counts[oldOwner]--;
+        state.counts[newOwner]++;
+        return state;
+      })
+    });
+  }
   render() {
+    const scoreButtons = [];
+    for (let group of Object.keys(colors)) {
+      scoreButtons.push(<ScoreButton key={group} group={group} value={this.state.counts[group]} />);
+    }
     return (
       <>
         <ResetButton />
-        <ScoreButton group="france" initialValue={7} />
-        <ScoreButton group="britain" initialValue={7} />
-        <ScoreButton group="mohawk" initialValue={4} />
-        <ScoreButton group="cherokee" initialValue={3} />
-        <ScoreButton group="shawnee" initialValue={4} />
-        <ScoreButton group="miami" initialValue={4} />
-        <ScoreButton group="ojibwe" initialValue={3} />
+        {scoreButtons}
       </>
     );
   }
 }
+Navbar.propTypes = {
+  initialOwnedHexes: PropTypes.object.isRequired,
+  eventPipe: PropTypes.instanceOf(EventPipe).isRequired,
+};
 export class ResetButton extends Component {
   render() {
     return (
@@ -32,12 +54,12 @@ export class ScoreButton extends Component {
   render() {
     return (
       <button className="navbar-button score-button" id={`score-button-${this.props.group}`}>
-        {capitalize(this.props.group)}: <span id={`score-${this.props.group}`}>{this.props.initialValue}</span>
+        {capitalize(this.props.group)}: <span id={`score-${this.props.group}`}>{this.props.value}</span>
       </button>
     );
   }
 }
 ScoreButton.propTypes = {
   group: PropTypes.string.isRequired,
-  initialValue: PropTypes.number.isRequired,
-}
+  value: PropTypes.number.isRequired,
+};
