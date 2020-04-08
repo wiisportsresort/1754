5f9afcf88aa854c5044ac4bd9dc0089abf8a3a1d
wiisportsresort <devdoge1@gmail.com>
2020-04-08 10:11:45 -0400

    minify stuff, stuff for wds

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 require('dotenv').config();
 
 const developmentEnabled = process.env.NODE_ENV === 'development';
 
-const devPlugins = developmentEnabled
+const envPlugins = developmentEnabled
   ? [
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
       }),
-      new MiniCSSExtractPlugin({
-        filename: '[name].css',
-        chunkFilename: '[id].css',
-      }),
-      // - webpack HMR only
-      // new webpack.HotModuleReplacementPlugin(),
-      // new webpack.NoEmitOnErrorsPlugin(),
     ]
-  : [];
+  : [new OptimizeCSSAssetsPlugin()];
 
-// - webpack HMR only
-// const entry = entryFile => developmentEnabled ? ['webpack-hot-middleware/client?overlay=true', entryFile] : entryFile
-
-module.exports = {
+const envOptimization = developmentEnabled
+  ? {
+      minimize: false,
+      minimizer: undefined,
+    }
+  : {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          test: /\.js(x?)$/i,
+          sourceMap: true,
+          extractComments: true,
+        }),
+      ],
+    };
+const webpackConfig = {
   mode: developmentEnabled ? 'development' : 'production',
   entry: {
     main: './src/main',
@@ -39,17 +45,66 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   context: __dirname,
-  stats: { children: false },
+  devServer: {
+    stats: {
+      colors: true,
+      children: false,
+    },
+  },
   devtool: developmentEnabled ? 'inline-source-map' : 'source-map',
   watchOptions: {
     ignored: ['public/**', 'node_modules/**'],
     aggregateTimeout: 0,
   },
-  plugins: [...devPlugins],
-  externals: {
-    jquery: '$',
-    react: 'React',
-    'react-dom': 'ReactDOM',
+  plugins: [
+    ...envPlugins,
+    new MiniCSSExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  optimization: {
+    ...envOptimization,
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      // minRemainingSize: 0,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        'vendor_react-dom': {
+          test: /[\\/]node_modules[\\/]react-dom[\\/]/,
+          name: 'vendor_react-dom',
+          chunks: 'all',
+          priority: 5,
+        },
+        vendor_react: {
+          test: /[\\/]node_modules[\\/]react[\\/]/,
+          name: 'vendor_react',
+          chunks: 'all',
+          priority: 5,
+        },
+        vendor_jquery: {
+          test: /[\\/]node_modules[\\/]jquery[\\/]/,
+          name: 'vendor_jquery',
+          chunks: 'all',
+          priority: 5,
+        },
+        vendor_other: {
+          test: /[\\/]node_modules[\\/](?!react|react-dom|jquery).*[\\/]/,
+          name: 'vendor_other',
+          chunks: 'all',
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   module: {
     rules: [
@@ -60,7 +115,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          developmentEnabled ? MiniCSSExtractPlugin.loader : 'style-loader',
+          MiniCSSExtractPlugin.loader,
           'css-loader',
           {
             loader: 'postcss-loader',
@@ -86,3 +141,6 @@ module.exports = {
     ],
   },
 };
+
+module.exports = { webpackConfig, developmentEnabled };
+// module.exports = webpackConfig;
