4c77c8dd46fbaa5ae5fdfbd53db4abe24488043b
wiisportsresort <devdoge1@gmail.com>
2020-05-02 22:38:28 -0400

    commit dist files

---
+++ dist/main.87dc24fdb773e53f4cdf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,a,e){e(1),n.exports=e(2)},function(n,a,e){"use strict";var d,u;d="Happy "+["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][(new Date).getDay()]+"!",null!==(u=document.querySelector("#day"))&&(u.innerHTML=d)},function(n,a,e){}],[[0,1]]]);
+//# sourceMappingURL=main.87dc24fdb773e53f4cdf.js.map
